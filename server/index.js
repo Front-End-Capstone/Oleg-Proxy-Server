6343082c16a2f854e8261d8c106eb9b5c416cd15
Oleg Rudenko <orudenkony@gmail.com>
2018-09-25 13:04:31 -0700

    Connecting all components with their databases, reviews is done

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const request = require('request');
 const app = express();
-
+const router = require('./router.js');
 const port = process.env.PORT || 8000;
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '../')));
-
+app.use('/', router);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
